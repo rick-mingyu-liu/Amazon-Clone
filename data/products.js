a8c95f132f250fed1e371709d9b5754acb9e79e1
rick-mingyu-liu <rickmyliu@gmail.com>
2024-05-08 15:16:00 -0400

    load products from the backend

--- data/products.js
+++ data/products.js
@@ -81,6 +81,32 @@ class Clothing extends Product{
 //   }
 // };
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () =>{
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      }
+    
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.open('GET', "https://supersimplebackend.dev/products");
+  xhr.send();
+}
+
+loadProducts();
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -740,10 +766,6 @@ export const products = [
       "mens"
     ]
   }
-].map((productDetails) => {
-  if (productDetails.type === "clothing") {
-    return new Clothing(productDetails);
-  }
+]
+*/
 
-  return new Product(productDetails);
-});
